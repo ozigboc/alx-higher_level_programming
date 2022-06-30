2e04256a2efe5cd81a9edb664067c653e7d6f965
ozigboc <ozigboc@gmail.com>
2022-06-30 20:09:17 +0100

    advance task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for n in range(4, 6):
             c = add(c, n)
-            return c
-        else:
-            return sub(a, b)
+        return c
+    else:
+        return sub(a, b)

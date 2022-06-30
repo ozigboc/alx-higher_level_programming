9ee2f377b0f3a6e471496600ce86d851992637dd
ozigboc <ozigboc@gmail.com>
2022-06-30 20:13:53 +0100

    advance task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,18 +9,18 @@ if __name__ == '__main__':
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        a = int(sys.argv[1])
-        op = sys.argv[2]
-        b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
 
-        if op == '+':
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-        elif op == '-':
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-        elif op == '*':
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-        elif op == '/':
-            print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)

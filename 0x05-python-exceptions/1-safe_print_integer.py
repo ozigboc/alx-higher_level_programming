7186e7b9fd5f2f5ad4f0a288f81c386ef1be86a2
ozigboc <ozigboc@gmail.com>
2022-07-19 00:03:51 +0200

    python task

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""function that checks and prints integer and handles valueerror exception"""
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False

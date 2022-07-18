80d1b2bc21b8845af0e35db461f203de0b0cf828
ozigboc <ozigboc@gmail.com>
2022-07-18 23:51:03 +0200

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,6 +13,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print()
                 return index
-            except IndexError:
-                print()
-                return index
+        except IndexError:
+            print()
+            return index

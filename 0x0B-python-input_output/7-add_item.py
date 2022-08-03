f206694812c0227cd77278813674a1fbeb38faee
ozigboc <ozigboc@gmail.com>
2022-08-03 20:40:49 +0800

    input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     try:
         new_list = load_from_json_file('add_item.json')
     except FileNotFoundError:
-        new_list = []
+       new_list = []
     new_list.extend(argv[1:])
     save_to_json_file(new_list, 'add_item.json')

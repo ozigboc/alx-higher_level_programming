7e837f8489fc94dea7cbbd9fc9f15f5f6d456ff5
ozigboc <ozigboc@gmail.com>
2022-10-06 14:31:46 +0100

    prm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a script that lists all the states
+from the database
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+            host='localhost', port=3306,
+            user=argv[1], password=argv[2], database=argv[3]
+            )
+    cur = con.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC;')
+
+    db = cur.fetchall()
+    for i in db:
+        print(i)
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument
+Displays all the values in states
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(
+            host='localhost', port=3306,
+            user=argv[1], password=argv[2],
+            database=argv[3]
+            )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE \
+            '{:s}' ORDER BY id ASC;".format(argv[4]))
+    db = cur.fetchall()
+
+    for i in db:
+        if i[1] == argv[4]:
+            print(i)

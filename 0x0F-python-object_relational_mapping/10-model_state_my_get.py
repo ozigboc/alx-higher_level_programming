7e4586db316dd9740f053daa6a17d9703ad64931
ozigboc <ozigboc@gmail.com>
2022-10-09 17:16:45 +0100

    orm

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-             'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                         argv[2],
                                                         argv[3]))
     Base.metadata.create_all(engine)

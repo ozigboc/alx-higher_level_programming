c98b72e2d84dbd331c503f50d50cc8a2a711d4c9
ozigboc <ozigboc@gmail.com>
2022-10-21 20:25:33 +0100

    network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,5 +11,5 @@ from sys import argv
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
     r = requests.get(url,
-                    auth=HTTPBasicAuth(argv[1], argv[2]))
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
